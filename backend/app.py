a55c39d817883acc63b5576a11369f0c7da6684a
naina-awan <f230777@cfd.nu.edu.pk>
2026-05-01 19:59:21 +0500

    update

--- backend/app.py
+++ backend/app.py
@@ -488,4 +488,5 @@ def get_state():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", debug=True, port=port)
